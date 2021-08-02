456261e3f37e207baf6b5861a5230d2d89a44895
yeseul choi <yschoi9930@gmail.com>
2021-08-03 00:48:34 +0900

    2-2. What is NLP

--- common/optimizer.py
+++ common/optimizer.py
@@ -1,22 +1,25 @@
 # coding: utf-8
-import numpy as np
+import sys
+sys.path.append('..')
+from common.np import *
+
 
 class SGD:
-
-    """확률적 경사 하강법（Stochastic Gradient Descent）"""
-
+    '''
+    확률적 경사하강법(Stochastic Gradient Descent)
+    '''
     def __init__(self, lr=0.01):
         self.lr = lr
         
     def update(self, params, grads):
-        for key in params.keys():
-            params[key] -= self.lr * grads[key] 
+        for i in range(len(params)):
+            params[i] -= self.lr * grads[i]
 
 
 class Momentum:
-
-    """모멘텀 SGD"""
-
+    '''
+    모멘텀 SGG(Momentum SGD)
+    '''
     def __init__(self, lr=0.01, momentum=0.9):
         self.lr = lr
         self.momentum = momentum
@@ -24,20 +27,20 @@ class Momentum:
         
     def update(self, params, grads):
         if self.v is None:
-            self.v = {}
-            for key, val in params.items():                                
-                self.v[key] = np.zeros_like(val)
-                
-        for key in params.keys():
-            self.v[key] = self.momentum*self.v[key] - self.lr*grads[key] 
-            params[key] += self.v[key]
+            self.v = []
+            for param in params:
+                self.v.append(np.zeros_like(param))
+
+        for i in range(len(params)):
+            self.v[i] = self.momentum * self.v[i] - self.lr * grads[i]
+            params[i] += self.v[i]
 
 
 class Nesterov:
-
-    """Nesterov's Accelerated Gradient (http://arxiv.org/abs/1212.0901)"""
-    # NAG는 모멘텀에서 한 단계 발전한 방법이다. (http://newsight.tistory.com/224)
-    
+    '''
+    네스테로프 가속 경사(NAG; Nesterov's Accelerated Gradient) (http://arxiv.org/abs/1212.0901)
+    '네스테로프 모멘텀 최적화'라고도 한다.
+    '''
     def __init__(self, lr=0.01, momentum=0.9):
         self.lr = lr
         self.momentum = momentum
@@ -45,40 +48,40 @@ class Nesterov:
         
     def update(self, params, grads):
         if self.v is None:
-            self.v = {}
-            for key, val in params.items():
-                self.v[key] = np.zeros_like(val)
-            
-        for key in params.keys():
-            self.v[key] *= self.momentum
-            self.v[key] -= self.lr * grads[key]
-            params[key] += self.momentum * self.momentum * self.v[key]
-            params[key] -= (1 + self.momentum) * self.lr * grads[key]
+            self.v = []
+            for param in params:
+                self.v.append(np.zeros_like(param))
+
+        for i in range(len(params)):
+            self.v[i] *= self.momentum
+            self.v[i] -= self.lr * grads[i]
+            params[i] += self.momentum * self.momentum * self.v[i]
+            params[i] -= (1 + self.momentum) * self.lr * grads[i]
 
 
 class AdaGrad:
-
-    """AdaGrad"""
-
+    '''
+    AdaGrad
+    '''
     def __init__(self, lr=0.01):
         self.lr = lr
         self.h = None
         
     def update(self, params, grads):
         if self.h is None:
-            self.h = {}
-            for key, val in params.items():
-                self.h[key] = np.zeros_like(val)
-            
-        for key in params.keys():
-            self.h[key] += grads[key] * grads[key]
-            params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-7)
+            self.h = []
+            for param in params:
+                self.h.append(np.zeros_like(param))
+
+        for i in range(len(params)):
+            self.h[i] += grads[i] * grads[i]
+            params[i] -= self.lr * grads[i] / (np.sqrt(self.h[i]) + 1e-7)
 
 
 class RMSprop:
-
-    """RMSprop"""
-
+    '''
+    RMSprop
+    '''
     def __init__(self, lr=0.01, decay_rate = 0.99):
         self.lr = lr
         self.decay_rate = decay_rate
@@ -86,20 +89,20 @@ class RMSprop:
         
     def update(self, params, grads):
         if self.h is None:
-            self.h = {}
-            for key, val in params.items():
-                self.h[key] = np.zeros_like(val)
-            
-        for key in params.keys():
-            self.h[key] *= self.decay_rate
-            self.h[key] += (1 - self.decay_rate) * grads[key] * grads[key]
-            params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-7)
+            self.h = []
+            for param in params:
+                self.h.append(np.zeros_like(param))
+
+        for i in range(len(params)):
+            self.h[i] *= self.decay_rate
+            self.h[i] += (1 - self.decay_rate) * grads[i] * grads[i]
+            params[i] -= self.lr * grads[i] / (np.sqrt(self.h[i]) + 1e-7)
 
 
 class Adam:
-
-    """Adam (http://arxiv.org/abs/1412.6980v8)"""
-
+    '''
+    Adam (http://arxiv.org/abs/1412.6980v8)
+    '''
     def __init__(self, lr=0.001, beta1=0.9, beta2=0.999):
         self.lr = lr
         self.beta1 = beta1
@@ -110,22 +113,16 @@ class Adam:
         
     def update(self, params, grads):
         if self.m is None:
-            self.m, self.v = {}, {}
-            for key, val in params.items():
-                self.m[key] = np.zeros_like(val)
-                self.v[key] = np.zeros_like(val)
+            self.m, self.v = [], []
+            for param in params:
+                self.m.append(np.zeros_like(param))
+                self.v.append(np.zeros_like(param))
         
         self.iter += 1
-        lr_t  = self.lr * np.sqrt(1.0 - self.beta2**self.iter) / (1.0 - self.beta1**self.iter)         
-        
-        for key in params.keys():
-            #self.m[key] = self.beta1*self.m[key] + (1-self.beta1)*grads[key]
-            #self.v[key] = self.beta2*self.v[key] + (1-self.beta2)*(grads[key]**2)
-            self.m[key] += (1 - self.beta1) * (grads[key] - self.m[key])
-            self.v[key] += (1 - self.beta2) * (grads[key]**2 - self.v[key])
+        lr_t = self.lr * np.sqrt(1.0 - self.beta2**self.iter) / (1.0 - self.beta1**self.iter)
+
+        for i in range(len(params)):
+            self.m[i] += (1 - self.beta1) * (grads[i] - self.m[i])
+            self.v[i] += (1 - self.beta2) * (grads[i]**2 - self.v[i])
             
-            params[key] -= lr_t * self.m[key] / (np.sqrt(self.v[key]) + 1e-7)
-            
-            #unbias_m += (1 - self.beta1) * (grads[key] - self.m[key]) # correct bias
-            #unbisa_b += (1 - self.beta2) * (grads[key]*grads[key] - self.v[key]) # correct bias
-            #params[key] += self.lr * unbias_m / (np.sqrt(unbisa_b) + 1e-7)
+            params[i] -= lr_t * self.m[i] / (np.sqrt(self.v[i]) + 1e-7)
--- common/trainer.py
+++ common/trainer.py
@@ -1,78 +1,172 @@
 # coding: utf-8
-import sys, os
-sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
-import numpy as np
-from common.optimizer import *
+import sys
+sys.path.append('..')
+import numpy
+import time
+import matplotlib.pyplot as plt
+from common.np import *  # import numpy as np
+from common.util import clip_grads
+
 
 class Trainer:
-    """신경망 훈련을 대신 해주는 클래스
-    """
-    def __init__(self, network, x_train, t_train, x_test, t_test,
-                 epochs=20, mini_batch_size=100,
-                 optimizer='SGD', optimizer_param={'lr':0.01}, 
-                 evaluate_sample_num_per_epoch=None, verbose=True):
-        self.network = network
-        self.verbose = verbose
-        self.x_train = x_train
-        self.t_train = t_train
-        self.x_test = x_test
-        self.t_test = t_test
-        self.epochs = epochs
-        self.batch_size = mini_batch_size
-        self.evaluate_sample_num_per_epoch = evaluate_sample_num_per_epoch
-
-        # optimzer
-        optimizer_class_dict = {'sgd':SGD, 'momentum':Momentum, 'nesterov':Nesterov,
-                                'adagrad':AdaGrad, 'rmsprpo':RMSprop, 'adam':Adam}
-        self.optimizer = optimizer_class_dict[optimizer.lower()](**optimizer_param)
-        
-        self.train_size = x_train.shape[0]
-        self.iter_per_epoch = max(self.train_size / mini_batch_size, 1)
-        self.max_iter = int(epochs * self.iter_per_epoch)
-        self.current_iter = 0
+    def __init__(self, model, optimizer):
+        self.model = model
+        self.optimizer = optimizer
+        self.loss_list = []
+        self.eval_interval = None
         self.current_epoch = 0
-        
-        self.train_loss_list = []
-        self.train_acc_list = []
-        self.test_acc_list = []
 
-    def train_step(self):
-        batch_mask = np.random.choice(self.train_size, self.batch_size)
-        x_batch = self.x_train[batch_mask]
-        t_batch = self.t_train[batch_mask]
-        
-        grads = self.network.gradient(x_batch, t_batch)
-        self.optimizer.update(self.network.params, grads)
-        
-        loss = self.network.loss(x_batch, t_batch)
-        self.train_loss_list.append(loss)
-        if self.verbose: print("train loss:" + str(loss))
-        
-        if self.current_iter % self.iter_per_epoch == 0:
+    def fit(self, x, t, max_epoch=10, batch_size=32, max_grad=None, eval_interval=20):
+        data_size = len(x)
+        max_iters = data_size // batch_size
+        self.eval_interval = eval_interval
+        model, optimizer = self.model, self.optimizer
+        total_loss = 0
+        loss_count = 0
+
+        start_time = time.time()
+        for epoch in range(max_epoch):
+            # 뒤섞기
+            idx = numpy.random.permutation(numpy.arange(data_size))
+            x = x[idx]
+            t = t[idx]
+
+            for iters in range(max_iters):
+                batch_x = x[iters*batch_size:(iters+1)*batch_size]
+                batch_t = t[iters*batch_size:(iters+1)*batch_size]
+
+                # 기울기 구해 매개변수 갱신
+                loss = model.forward(batch_x, batch_t)
+                model.backward()
+                params, grads = remove_duplicate(model.params, model.grads)  # 공유된 가중치를 하나로 모음
+                if max_grad is not None:
+                    clip_grads(grads, max_grad)
+                optimizer.update(params, grads)
+                total_loss += loss
+                loss_count += 1
+
+                # 평가
+                if (eval_interval is not None) and (iters % eval_interval) == 0:
+                    avg_loss = total_loss / loss_count
+                    elapsed_time = time.time() - start_time
+                    print('| 에폭 %d |  반복 %d / %d | 시간 %d[s] | 손실 %.2f'
+                          % (self.current_epoch + 1, iters + 1, max_iters, elapsed_time, avg_loss))
+                    self.loss_list.append(float(avg_loss))
+                    total_loss, loss_count = 0, 0
+
             self.current_epoch += 1
-            
-            x_train_sample, t_train_sample = self.x_train, self.t_train
-            x_test_sample, t_test_sample = self.x_test, self.t_test
-            if not self.evaluate_sample_num_per_epoch is None:
-                t = self.evaluate_sample_num_per_epoch
-                x_train_sample, t_train_sample = self.x_train[:t], self.t_train[:t]
-                x_test_sample, t_test_sample = self.x_test[:t], self.t_test[:t]
-                
-            train_acc = self.network.accuracy(x_train_sample, t_train_sample)
-            test_acc = self.network.accuracy(x_test_sample, t_test_sample)
-            self.train_acc_list.append(train_acc)
-            self.test_acc_list.append(test_acc)
 
-            if self.verbose: print("=== epoch:" + str(self.current_epoch) + ", train acc:" + str(train_acc) + ", test acc:" + str(test_acc) + " ===")
-        self.current_iter += 1
+    def plot(self, ylim=None):
+        x = numpy.arange(len(self.loss_list))
+        if ylim is not None:
+            plt.ylim(*ylim)
+        plt.plot(x, self.loss_list, label='train')
+        plt.xlabel('반복 (x' + str(self.eval_interval) + ')')
+        plt.ylabel('손실')
+        plt.show()
 
-    def train(self):
-        for i in range(self.max_iter):
-            self.train_step()
 
-        test_acc = self.network.accuracy(self.x_test, self.t_test)
+class RnnlmTrainer:
+    def __init__(self, model, optimizer):
+        self.model = model
+        self.optimizer = optimizer
+        self.time_idx = None
+        self.ppl_list = None
+        self.eval_interval = None
+        self.current_epoch = 0
 
-        if self.verbose:
-            print("=============== Final Test Accuracy ===============")
-            print("test acc:" + str(test_acc))
+    def get_batch(self, x, t, batch_size, time_size):
+        batch_x = np.empty((batch_size, time_size), dtype='i')
+        batch_t = np.empty((batch_size, time_size), dtype='i')
 
+        data_size = len(x)
+        jump = data_size // batch_size
+        offsets = [i * jump for i in range(batch_size)]  # 배치에서 각 샘플을 읽기 시작하는 위치
+
+        for time in range(time_size):
+            for i, offset in enumerate(offsets):
+                batch_x[i, time] = x[(offset + self.time_idx) % data_size]
+                batch_t[i, time] = t[(offset + self.time_idx) % data_size]
+            self.time_idx += 1
+        return batch_x, batch_t
+
+    def fit(self, xs, ts, max_epoch=10, batch_size=20, time_size=35,
+            max_grad=None, eval_interval=20):
+        data_size = len(xs)
+        max_iters = data_size // (batch_size * time_size)
+        self.time_idx = 0
+        self.ppl_list = []
+        self.eval_interval = eval_interval
+        model, optimizer = self.model, self.optimizer
+        total_loss = 0
+        loss_count = 0
+
+        start_time = time.time()
+        for epoch in range(max_epoch):
+            for iters in range(max_iters):
+                batch_x, batch_t = self.get_batch(xs, ts, batch_size, time_size)
+
+                # 기울기를 구해 매개변수 갱신
+                loss = model.forward(batch_x, batch_t)
+                model.backward()
+                params, grads = remove_duplicate(model.params, model.grads)  # 공유된 가중치를 하나로 모음
+                if max_grad is not None:
+                    clip_grads(grads, max_grad)
+                optimizer.update(params, grads)
+                total_loss += loss
+                loss_count += 1
+
+                # 퍼플렉서티 평가
+                if (eval_interval is not None) and (iters % eval_interval) == 0:
+                    ppl = np.exp(total_loss / loss_count)
+                    elapsed_time = time.time() - start_time
+                    print('| 에폭 %d |  반복 %d / %d | 시간 %d[s] | 퍼플렉서티 %.2f'
+                          % (self.current_epoch + 1, iters + 1, max_iters, elapsed_time, ppl))
+                    self.ppl_list.append(float(ppl))
+                    total_loss, loss_count = 0, 0
+
+            self.current_epoch += 1
+
+    def plot(self, ylim=None):
+        x = numpy.arange(len(self.ppl_list))
+        if ylim is not None:
+            plt.ylim(*ylim)
+        plt.plot(x, self.ppl_list, label='train')
+        plt.xlabel('반복 (x' + str(self.eval_interval) + ')')
+        plt.ylabel('퍼플렉서티')
+        plt.show()
+
+
+def remove_duplicate(params, grads):
+    '''
+    매개변수 배열 중 중복되는 가중치를 하나로 모아
+    그 가중치에 대응하는 기울기를 더한다.
+    '''
+    params, grads = params[:], grads[:]  # copy list
+
+    while True:
+        find_flg = False
+        L = len(params)
+
+        for i in range(0, L - 1):
+            for j in range(i + 1, L):
+                # 가중치 공유 시
+                if params[i] is params[j]:
+                    grads[i] += grads[j]  # 경사를 더함
+                    find_flg = True
+                    params.pop(j)
+                    grads.pop(j)
+                # 가중치를 전치행렬로 공유하는 경우(weight tying)
+                elif params[i].ndim == 2 and params[j].ndim == 2 and \
+                     params[i].T.shape == params[j].shape and np.all(params[i].T == params[j]):
+                    grads[i] += grads[j].T
+                    find_flg = True
+                    params.pop(j)
+                    grads.pop(j)
+
+                if find_flg: break
+            if find_flg: break
+
+        if not find_flg: break
+
+    return params, grads
